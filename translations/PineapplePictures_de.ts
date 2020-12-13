16c0afc8f13f34c69992ddf18e5f6368a1924e6e
K. Herbert <herbert.ka@mailo.com>
2020-12-13 20:47:44 +0000

    Translated using Weblate (German)
    Currently translated at 31.4% (33 of 105 strings)
    
    Translation: pineapple-pictures/Application
    Translate-URL: https://hosted.weblate.org/projects/pineapple-pictures/application/de/

--- translations/PineapplePictures_de.ts
+++ translations/PineapplePictures_de.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../app/aboutdialog.cpp" line="22"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Über</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="25"/>
         <source>Launch application with image file path as argument to load the file.</source>
-        <translation type="unfinished"></translation>
+        <translation>Starten Sie die Anwendung mit dem Bilddateipfad als Argument zum Laden der Datei.</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="26"/>
@@ -36,127 +36,127 @@
     <message>
         <location filename="../app/aboutdialog.cpp" line="44"/>
         <source>Version: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Version: %1</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="47"/>
         <source>Copyright (c) 2020 %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Copyright © 2020 %1</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="49"/>
         <source>Logo designed by %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Logo entworfen von %1</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="51"/>
         <source>Built with Qt %1 (%2)</source>
-        <translation type="unfinished"></translation>
+        <translation>Gemacht mit Qt %1 (%2)</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="52"/>
         <source>Source code</source>
-        <translation type="unfinished"></translation>
+        <translation>Quellcode</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="58"/>
         <source>Contributors</source>
-        <translation type="unfinished"></translation>
+        <translation>Mitwirkenden</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="60"/>
         <source>List of contributors on GitHub</source>
-        <translation type="unfinished"></translation>
+        <translation>Liste der Mitwirkenden auf GitHub</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="61"/>
         <source>Thanks to all people who contributed to this project.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vielen Dank an alle, die zu diesem Projekt beigetragen haben.</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="65"/>
         <source>Translators</source>
-        <translation type="unfinished"></translation>
+        <translation>Übersetzer</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="66"/>
         <source>I would like to thank the following people who volunteered to translate this application.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ich möchte den folgenden Personen danken, die sich freiwillig zur Übersetzung dieser Anwendung gemeldet haben.</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="134"/>
         <source>&amp;Special Thanks</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Besonderer Dank</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="136"/>
         <source>&amp;Third-party Libraries</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Bibliotheken von Drittanbietern</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="72"/>
         <source>Your Rights</source>
-        <translation type="unfinished"></translation>
+        <translation>Ihre Rechte</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="74"/>
         <source>%1 is released under the MIT License.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 wird unter der MIT-Lizenz veröffentlicht.</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="75"/>
         <source>This license grants people a number of freedoms:</source>
-        <translation type="unfinished"></translation>
+        <translation>Diese Lizenz gewährt Menschen eine Reihe von Freiheiten:</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="76"/>
         <source>You are free to use %1, for any purpose</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie dürfen %1 für jeden Zweck verwenden</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="77"/>
         <source>You are free to distribute %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie dürfen %1 verteilen</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="78"/>
         <source>You can study how %1 works and change it</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie können untersuchen, wie %1 funktioniert, und es ändern</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="79"/>
         <source>You can distribute changed versions of %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie können geänderte Versionen von %1 verteilen</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="81"/>
         <source>The MIT license guarantees you this freedom. Nobody is ever permitted to take it away.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die MIT-Lizenz garantiert Ihnen diese Freiheit. Niemand darf es jemals wegnehmen.</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="109"/>
         <source>Third-party Libraries used by %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Von %1 verwendete Bibliotheken von Drittanbietern</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="110"/>
         <source>%1 is built on the following free software libraries:</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 basiert auf den folgenden freien Softwarebibliotheken:</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="132"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Hilfe</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="133"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Über</translation>
     </message>
     <message>
         <location filename="../app/aboutdialog.cpp" line="135"/>
         <source>&amp;License</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Lizenz</translation>
     </message>
 </context>
 <context>
@@ -164,7 +164,7 @@
     <message>
         <location filename="../app/graphicsscene.cpp" line="16"/>
         <source>Drag image here</source>
-        <translation type="unfinished"></translation>
+        <translation>Ziehen Sie das Bild hierher</translation>
     </message>
 </context>
 <context>
@@ -172,12 +172,12 @@
     <message>
         <location filename="../app/graphicsview.cpp" line="261"/>
         <source>File url list is empty</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Datei-URL-Liste ist leer</translation>
     </message>
     <message>
         <location filename="../app/graphicsview.cpp" line="53"/>
         <source>File is not a valid image</source>
-        <translation type="unfinished"></translation>
+        <translation>Datei ist kein gültiges Bild</translation>
     </message>
     <message>
         <location filename="../app/graphicsview.cpp" line="55"/>
@@ -282,7 +282,7 @@
         <location filename="../app/metadatamodel.cpp" line="43"/>
         <source>File</source>
         <comment>Section name.</comment>
-        <translation type="unfinished"></translation>
+        <translation>Datei</translation>
     </message>
     <message>
         <location filename="../app/metadatamodel.cpp" line="38"/>
@@ -293,7 +293,7 @@
     <message>
         <location filename="../app/metadatamodel.cpp" line="28"/>
         <source>%1 File</source>
-        <translation type="unfinished"></translation>
+        <translation>%1-Datei</translation>
     </message>
     <message>
         <location filename="../app/metadatamodel.cpp" line="35"/>
@@ -557,7 +557,7 @@
     <message>
         <location filename="../app/main.cpp" line="31"/>
         <source>File list.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateiliste.</translation>
     </message>
 </context>
 </TS>
